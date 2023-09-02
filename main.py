305ef816e241b5500a10535f0f968d4fed5ab54c
Shoko Ishigaki <shoko.ishigaki@gmail.com>
2023-09-02 20:32:40 +0900

    Initial Commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+from dotenv import load_dotenv
+
+from fastapi import FastAPI, Header, Request, HTTPException
+
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+
+load_dotenv()
+
+configuration = Configuration(access_token=os.environ['CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+
+app = FastAPI()
+
+@app.post("/callback")
+async def callback(request: Request, x_line_signature=Header(None)):
+    body = await request.body()
+    try:
+        handler.handle(body.decode('utf-8'), x_line_signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="InvalidSignatureError")
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
