c4c593f1935bc7ca7bb3bc294445aad708681931
Shoko Ishigaki <shoko.ishigaki@gmail.com>
2023-09-13 19:17:21 +0900

    QAの場合の処理追加

--- main.py
+++ main.py
@@ -14,7 +14,10 @@ from linebot.v3.messaging import (
     ApiClient,
     MessagingApi,
     ReplyMessageRequest,
-    TextMessage
+    TextMessage,
+    PostbackAction,
+    ButtonsTemplate,
+    TemplateMessage
 )
 from linebot.v3.webhooks import (
     MessageEvent,
@@ -41,12 +44,37 @@ async def callback(request: Request, x_line_signature=Header(None)):
 
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    with ApiClient(configuration) as api_client:
-        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": event.message.text}])
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=completion.choices[0].message.content)]
+    if event.message.text == "QA":
+        with ApiClient(configuration) as api_client:
+            line_bot_api = MessagingApi(api_client)
+            buttons_template_message = TemplateMessage(
+                alt_text='QA',
+                template=ButtonsTemplate(
+                    title='QA',
+                    text='質問したい内容を入力してください',
+                    actions=[
+                        PostbackAction(
+                            data='test',
+                            label='メッセージを送信',
+                            input_option='openKeyboard'
+                        )
+                    ]
+                )
+            )    
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[buttons_template_message]
+                )
             )
-        )
+    else:
+        with ApiClient(configuration) as api_client:
+            openai.api_key = os.environ['OPENAI_API_KEY']
+            completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": event.message.text}])
+            line_bot_api = MessagingApi(api_client)
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[TextMessage(text=completion.choices[0].message.content)]
+                )
+            )
